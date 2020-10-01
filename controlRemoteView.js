9c092f09bc30da3ac4c8067ff953fab22e6fc786
nyandora <61850164+nyandora@users.noreply.github.com>
2020-10-01 19:43:17 +0900

    Update controlRemoteView.js

--- controlRemoteView.js
+++ controlRemoteView.js
@@ -1,15 +1,15 @@
 document.addEventListener("keydown", e => {
   if ((e.ctrlKey
-      && (e.keyCode === 83
-        || e.keyCode === 70
-        || (e.shiftKey && e.keyCode === 79)
-        || e.keyCode === 33
-        || e.keyCode === 34
+      && (e.keyCode === 83 // Ctrl + S
+        || e.keyCode === 70 // Ctrl + F
+        || (e.shiftKey && e.keyCode === 79) // Ctrl + Shift + o
+        || e.keyCode === 33 // PgUp
+        || e.keyCode === 34 // PgDn
     ))
-    || e.keyCode === 114
-    || e.keyCode === 115
-    || e.keyCode === 116
-    || e.keyCode === 123
+    || e.keyCode === 114 // F3
+    || e.keyCode === 115 // F4
+    || e.keyCode === 116 // F5
+    || e.keyCode === 123 // F12
   ) {
     e.preventDefault();
   }
