8e83f048f02cef82f9cf8b7e2b858493a1f97d77
nyandora <61850164+nyandora@users.noreply.github.com>
2020-12-11 16:25:00 +0900

    Create login.js

---
+++ RemoteWorks/login.js
@@ -0,0 +1,4 @@
+// 対象ホスト：browser.remoteworks.jp
+$("#tis-container-wrap").find("#bizCode").val("P2430");
+$("#tis-container-wrap").find("#userId").val("社員番号");
+$("#tis-container-wrap").find("#tis-password").val("パスワード");
